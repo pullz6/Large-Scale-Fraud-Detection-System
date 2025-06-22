122ba292ff02b4a447201ad1c41ea03a1041a446
Sara <gunawardhanapulsara@gmail.com>
2025-06-22 14:45:04 +0100

    initiate

---
+++ scripts/preprocessing.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import random
+from sklearn import preprocessing
+
+import os
+from kaggle.api.kaggle_api_extended import KaggleApi
+import json
+import mlflow
+from datetime import datetime, timedelta
+
+print('Testing - 01')
