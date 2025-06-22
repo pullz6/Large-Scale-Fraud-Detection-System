5d1c5f158cae12fc36ffc7814165cb153dd5dc6b
Sara <gunawardhanapulsara@gmail.com>
2025-06-22 14:58:26 +0100

    dataset link made

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import random
 from sklearn import preprocessing
+import kaggle
 
 import os
 from kaggle.api.kaggle_api_extended import KaggleApi
@@ -9,4 +10,6 @@ import json
 import mlflow
 from datetime import datetime, timedelta
 
-print('Testing - 01')
+
+kaggle.api.authenticate()
+kaggle.api.dataset_download_files('kartik2112/fraud-detection', path='data/', unzip=True)
