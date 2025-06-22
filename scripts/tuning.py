c5be2780a23feed36859f2f303f36e7ee51f8e4f
Sara <gunawardhanapulsara@gmail.com>
2025-06-22 20:22:00 +0100

    fix: optuna added

---
+++ scripts/tuning.py
@@ -0,0 +1,50 @@
+import optuna
+import sklearn
+import xgboost as xgb
+import numpy as np 
+
+def objective(trial, dtrain, dvalid, y_test):
+    param = {
+        "verbosity": 0,
+        "objective": "binary:logistic",
+        # use exact for small dataset.
+        "tree_method": "exact",
+        # defines booster, gblinear for linear functions.
+        "booster": trial.suggest_categorical("booster", ["gbtree", "gblinear", "dart"]),
+        # L2 regularization weight.
+        "lambda": trial.suggest_float("lambda", 1e-8, 1.0, log=True),
+        # L1 regularization weight.
+        "alpha": trial.suggest_float("alpha", 1e-8, 1.0, log=True),
+        # sampling ratio for training data.
+        "subsample": trial.suggest_float("subsample", 0.2, 1.0),
+        # sampling according to each tree.
+        "colsample_bytree": trial.suggest_float("colsample_bytree", 0.2, 1.0),
+    }
+
+    if param["booster"] in ["gbtree", "dart"]:
+        # maximum depth of the tree, signifies complexity of the tree.
+        param["max_depth"] = trial.suggest_int("max_depth", 3, 9, step=2)
+        # minimum child weight, larger the term more conservative the tree.
+        param["min_child_weight"] = trial.suggest_int("min_child_weight", 2, 10)
+        param["eta"] = trial.suggest_float("eta", 1e-8, 1.0, log=True)
+        # defines how selective algorithm is.
+        param["gamma"] = trial.suggest_float("gamma", 1e-8, 1.0, log=True)
+        param["grow_policy"] = trial.suggest_categorical("grow_policy", ["depthwise", "lossguide"])
+
+    if param["booster"] == "dart":
+        param["sample_type"] = trial.suggest_categorical("sample_type", ["uniform", "weighted"])
+        param["normalize_type"] = trial.suggest_categorical("normalize_type", ["tree", "forest"])
+        param["rate_drop"] = trial.suggest_float("rate_drop", 1e-8, 1.0, log=True)
+        param["skip_drop"] = trial.suggest_float("skip_drop", 1e-8, 1.0, log=True)
+
+    bst = xgb.train(param, dtrain)
+    preds = bst.predict(dvalid)
+    pred_labels = np.rint(preds)
+    accuracy = sklearn.metrics.accuracy_score(y_test, pred_labels)
+    return accuracy
+
+def find_best_parameters(dmatrix_train,dmatrix_test, y_test): 
+    study = optuna.create_study(direction="maximize")
+    study.optimize(lambda trial: objective(trial,dmatrix_train, dmatrix_test, y_test), n_trials=100, timeout=600)
+    best_parameters = study.best_trial.params
+    return best_parameters
